b6c0a6e4ffb57a328f407fbcd3974d5499bea438
Roman Pecheniuk <pechenyuknet@gmail.com>
2026-05-19 16:28:11 +0000

    daily weather model created

---
+++ models/daily_weather.sql
@@ -0,0 +1,23 @@
+with cte_daily_weather as (
+    select date(time) as daily_weather,
+           weather,
+           temp,
+           pressure,
+           humidity,
+           clouds
+    from {{ source('demo', 'weather') }}
+    --limit 10
+), cte_daily_weather_agg as (
+    select daily_weather,
+           weather,
+           round(avg(temp), 2) as avg_temp,
+           round(avg(pressure), 2) as avg_pressure,
+           round(avg(humidity), 2) as avg_humidity,
+           round(avg(clouds), 2) as avg_clouds
+    from cte_daily_weather
+    group by daily_weather,
+             weather
+    qualify row_number() over(partition by daily_weather order by count(weather) desc) = 1             
+)
+select *
+from cte_daily_weather_agg
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,15 +1,9 @@
 with cte as (
-    select  to_timestamp(started_at) as started_at,
+    select  to_timestamp(started_at)       as started_at,
             date(to_timestamp(started_at)) as date_started_at,
             hour(to_timestamp(started_at)) as hour_started_at,
-            case when dayname(to_timestamp(started_at)) in ('Sat', 'Sum') then 'WEEKEND'
-            ELSE 'BUSINESS' END as day_type,
-            case 
-                when month(to_timestamp(started_at)) in (12, 1, 2)  then 'winter'
-                when month(to_timestamp(started_at)) in (3, 4, 5)   then 'spring'
-                when month(to_timestamp(started_at)) in (6, 7, 8)   then 'summer'
-                when month(to_timestamp(started_at)) in (9, 10, 11) then 'autumn'
-            end as STATION_OF_YEAR
+            {{day_type('started_at')}} as fn_day_type,
+            {{get_season('started_at')}} as STATION_OF_YEAR
             
     from {{ source('demo', 'bike') }}   
     where started_at <> 'started_at'
