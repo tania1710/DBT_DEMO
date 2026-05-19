3453c8cdea60f3f0dd4982afe600ac04ada009a6
Roman Pecheniuk <pechenyuknet@gmail.com>
2026-05-19 17:02:38 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,12 @@
+with cte_trip as (
+    select ride_id,
+           rideable_type,
+           date(to_timestamp(started_at)) as trip_date,
+           START_STATIO_ID as START_STATION_ID,
+           end_STATIOn_name as end_STATION_name,
+           MEMBER_CSUAL AS MEMBER_CASUAL,
+           TIMESTAMPDIFF(SECOND, to_timestamp(started_at), to_timestamp(ENDED_at))
+    from {{ source('demo', 'bike') }}
+)
+select *
+from cte_trip
